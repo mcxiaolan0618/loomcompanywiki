0e16af77825baa01f25947fae26613a17385dd44
mcxiaolan0618 <kezhaoyi2@gmail.com>
2026-09-02 18:06:54 +0800

    fix

--- data/egos.js
+++ data/egos.js
@@ -10,7 +10,7 @@ const EGOS_INDEX = [
     { id: '萨尔温-认知失调理论', owner: '萨尔温', name: '认知失调理论', level: 'ZAYIN', obtain: '初始' },
     { id: '墨严卿-海德之镰',   owner: '墨严卿', name: '海德之镰',   level: 'ZAYIN', obtain: '初始' },
     { id: '帕菲-诸行无常',     owner: '帕菲',   name: '诸行无常',   level: 'ZAYIN', obtain: '初始' },
-    { id: '珂赛特-替罪羔羊',   owner: '珂赛特', name: '替罪之羊',   level: 'ZAYIN', obtain: '初始' },
+    { id: '珂赛特-替罪羔羊',   owner: '珂赛特', name: '替罪羔羊',   level: 'ZAYIN', obtain: '初始' },
     { id: '加拉哈德-湖中圣域', owner: '加拉哈德', name: '湖中圣域', level: 'ZAYIN', obtain: '初始' },
     { id: '爱德蒙-十字架',     owner: '爱德蒙', name: '十字架',     level: 'ZAYIN', obtain: '初始' },
     { id: '于连-律师徽章',     owner: '于连',   name: '律师徽章',   level: 'ZAYIN', obtain: '初始' },
