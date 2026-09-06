6f8e60fd2c2e8f67e63c7218d30814dcbb85252e
mcxiaolan0618 <kezhaoyi2@gmail.com>
2026-09-06 21:42:37 +0800

    fix

--- data/identities.js
+++ data/identities.js
@@ -69,15 +69,15 @@ const IDENTITIES_INDEX = [
         extract: '常驻',
         keywords: ['织机公司', 'LCR'],
         resist: {
-            slash:  { physics: '斩击', resistance: '2.0' },
-            pierce: { physics: '突刺', resistance: '0.5' },
-            blunt:  { physics: '打击', resistance: '1.0' }
+            slash:  { physics: '斩击', resistance: '[×1.0]' },
+            pierce: { physics: '突刺', resistance: '[×0.5]' },
+            blunt:  { physics: '打击', resistance: '[×2.0]' }
         },
-        skillTop:    { physics: '突刺', sin: '暴食' },
-        skillMiddle: { physics: '打击', sin: '怠惰' },
-        skillBottom: { physics: '斩击', sin: '色欲' },
+        skillTop:    { physics: '突刺', sin: '暴怒' },
+        skillMiddle: { physics: '突刺', sin: '暴食' },
+        skillBottom: { physics: '突刺', sin: '嫉妒' },
         combatPassive:  { sin: '暴食', way: '持有', need: '3' },
-        supportPassive: { sin: '怠惰', way: '共鸣', need: '2' }
+        supportPassive: { sin: '暴食', way: '持有', need: '6' }
     },
     {
         id: 'LCR员工-伍符',
